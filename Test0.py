dac0eab0206c7f616b3cce79fab8bae059cf8bf8
kuvaldastalina <m.kopytenko@yandex.ru>
2024-07-17 23:58:28 +0300

    Add files via upload

---
+++ Test0.py
@@ -0,0 +1,13 @@
+# //"1st Program"
+print(9**0.5*5)
+
+# //'2nd program'
+print(9.99 > 9.98 and 1000 != 1001)
+
+# //'3rd program'
+print((1234%1000//10) + (5678%1000//10))
+
+# //'4th program'
+print((type(int(42.13))) == (type(int(13.42))) or (type(float(13.42))) == (type(float(42.42))))
+
+
